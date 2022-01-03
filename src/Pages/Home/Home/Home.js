3a94a457370b7b8320e0e3e83418731801eb533c
unknown <rjmahir.faisal@gmail.com>
2022-01-04 00:34:09 +0600

    footer added on home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationBar from '../../Shared/NavigationBar/NavigationBar';
+import Footer from '../../Shared/Footer/Footer';
 import AboutUs from '../AboutUs/AboutUs';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
@@ -13,7 +14,7 @@ const Home = () => {
             <HomeProducts></HomeProducts>
             <AboutUs></AboutUs>
             <Contact></Contact>
-
+            <Footer></Footer>
         </div>
     );
 };
